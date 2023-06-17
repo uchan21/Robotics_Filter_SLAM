5aa06dc82e80664e66cc2aa604ea982c056f94a8
정유찬 <81511939+uchan21@users.noreply.github.com>
2023-06-17 18:52:08 +0900

    Delete Kalman_Filtering.m

--- Kalman_Filter/Kalman_Filtering.m
+++
@@ -1,8 +0,0 @@
-function [M_nowState,S_nowState] = Kalman_Filtering(M_preState,S_preState, M_preMotion, S_preMotion, M_nowMeas, S_nowMeas)
-    x = [-20:.1:200];
-    [M_predic,S_predic] = State_prediction(M_preState,S_preState,M_preMotion,S_preMotion);
-    [M_nowState,S_nowState] = Measurement_update(M_predic,S_predic, M_nowMeas,S_nowMeas);
-
-    y = normpdf(x,M_nowState,S_nowState);
-    plot(x,y,"g"); hold on; % Filtering plot
-end
