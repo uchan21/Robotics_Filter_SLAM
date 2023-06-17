6246eb8b99994f20223e9502ae8b1bff24f289a4
정유찬 <81511939+uchan21@users.noreply.github.com>
2023-06-17 18:51:58 +0900

    Delete Measurement_update.m

--- Kalman_Filter/Measurement_update.m
+++
@@ -1,8 +0,0 @@
-function [m,s] = Measurement_update(m,s,M_meas,S_meas)
-    x = [-20:.1:200];
-    y = normpdf(x,M_meas,S_meas);
-    plot(x,y,"b"); hold on; % Measurement plot
-
-    m = (M_meas*s+m*S_meas)/(s+S_meas);
-    s = (s*S_meas)/(s+S_meas);
-end
