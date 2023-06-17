2ecfc7c0aa1853d5945c8308b74d3cdb091d6bf0
정유찬 <81511939+uchan21@users.noreply.github.com>
2023-06-17 18:52:15 +0900

    Delete State_prediction.m

--- Kalman_Filter/State_prediction.m
+++
@@ -1,8 +0,0 @@
-function [m,s] = State_prediction(m, s, M_motion,S_motion)
-    x = [-20:.1:200];
-    m = m+M_motion;
-    s = s+S_motion;
-
-    y = normpdf(x,m,s);
-    plot(x,y,"R"); hold on; % State_prediction plot
-end
